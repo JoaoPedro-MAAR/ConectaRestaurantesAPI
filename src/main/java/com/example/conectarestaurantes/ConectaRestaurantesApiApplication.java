40488a5275682bb1fbfeb07c20064bf0e4834ce4
João Pedro Marques de Araújo Aragão Rodrigues <2005joaopedromaar@gmail.com>
2025-08-12 20:55:48 -0300

    Initial project setup with Spring Boot
    Add base Spring Boot application structure, including Maven wrapper, build configuration, main application class, test class, and application properties. Project is configured for web and webflux support, with Thymeleaf and Lombok dependencies.

---
+++ src/main/java/com/example/conectarestaurantes/ConectaRestaurantesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.conectarestaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaRestaurantesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConectaRestaurantesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/conectarestaurantes/ConectaRestaurantesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conectarestaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaRestaurantesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
