dee1d498b0ad98933b1050fcf9a98a76bcd6d599
LucasToscano22 <lucas.toscano85@gmail.com>
2025-12-14 12:22:59 -0300

    criacao dos model funcionario e empresa

---
+++ src/main/java/com/example/conectarestaurantes/model/Funcionario.java
@@ -0,0 +1,34 @@
+package com.example.conectarestaurantes.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_funcionario")
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf; 
+
+    @Column(nullable = false)
+    private String cargo;
+
+    @ManyToOne
+    @JoinColumn(name = "empresa_id", nullable = false)
+    private Empresa empresa;
+}
